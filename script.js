abab69df412d311566f717305753982ddc651203
hey1mryan <ryancai7519@gmail.com>
2024-08-04 21:56:37 -0400

    commit 2.0

---
+++ script.js
@@ -0,0 +1,13 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const navItems = document.querySelectorAll('item');
+
+//     navItems.forEach(item => {
+//         item.addEventListener('mouseover', function() {
+//             this.style.opacity = '0.5';
+//         });
+
+//         item.addEventListener('mouseout', function() {
+//             this.style.opacity = '1';
+//         });
+//     });
+// });
