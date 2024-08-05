c655272bb5470fdec04bf813403b133f0b1cfe6a
hey1mryan <ryancai7519@gmail.com>
2024-08-05 09:07:32 -0400

    Commit 3.2

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navItems = document.querySelectorAll('nav a');
 
-    scrollToTopLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
-
     navItems.forEach(item => {
         item.addEventListener('mouseover', function() {
             this.style.opacity = '0.25';
